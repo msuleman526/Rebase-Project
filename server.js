2f4e188e005ad1a0a417a75b85d59e01e376bd9f
msuleman526 <msuleman526@gmail.com>
2023-04-04 23:15:53 +0500

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const PORT = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') return respondHello(req, res)
+  if (req.url === '/user-agent') return userAgent(req, res)
 
   if (req.url === '/base64/:text') return base64Call(req, res)
   res.end()
 })
+
+function userAgent (req, res) {
+  res.send(req.headers['user-agent']);
+}
 
 function respondHello (req, res) {
   res.end(JSON.stringify({ msg: 'hello' }))
